d9b6b0953ab16b9cce62a3fd713c4eb18afc687a
eauge <estanislauauge@gmail.com>
2019-07-08 13:22:46 -0700

    create file per log

--- src/bin/oasis/config.rs
+++ src/bin/oasis/config.rs
@@ -2,10 +2,13 @@
 pub struct Logging {
     pub path_stdout: String,
     pub path_stderr: String,
+    pub dir: String,
     pub enabled: bool,
 }
 
 #[derive(Clone, Debug)]
 pub struct Config {
+    pub timestamp: i64,
+    pub id: u64,
     pub logging: Logging,
 }
