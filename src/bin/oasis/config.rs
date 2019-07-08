66511cde050376bf513d93002d2c1ee3304f62c7
eauge <estanislauauge@gmail.com>
2019-07-08 11:21:58 -0700

    add configuration

--- src/bin/oasis/config.rs
+++ src/bin/oasis/config.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+pub struct Wallet {
+    pub private_key: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct Profile {
+    pub wallet: Wallet,
+}
+
+#[derive(Clone, Debug)]
+pub struct Profiles {
+    pub profiles: HashMap<String, Profile>,
+    pub default: String,
+}
+
 #[derive(Clone, Debug)]
 pub struct Logging {
     pub path_stdout: String,
@@ -7,5 +25,12 @@ pub struct Logging {
 
 #[derive(Clone, Debug)]
 pub struct Config {
+    pub profiles: Profile,
     pub logging: Logging,
+}
+
+impl Config {
+    fn load() -> Result<Self, failure::Error> {
+        
+    }
 }
