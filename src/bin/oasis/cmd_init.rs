0198db333ce8c9ecdeb9bea9308f10d46a58a332
eauge <estanislauauge@gmail.com>
2019-07-10 10:15:51 -0700

    Add logging infrastructure (#33)

--- src/bin/oasis/cmd_init.rs
+++ src/bin/oasis/cmd_init.rs
@@ -1,18 +1,19 @@
-use crate::utils::{run_cmd, Verbosity};
+use crate::{
+    command::{run_cmd, Verbosity},
+    config::Config,
+    error::Error,
+};
 
 /// Creates an Oasis project in a directory.
-pub fn init(dir: &str, project_type: &str) -> Result<(), failure::Error> {
+pub fn init(config: &Config, dir: &str, project_type: &str) -> Result<(), failure::Error> {
     match project_type {
-        "rust" => init_rust(dir),
-        _ => Err(failure::format_err!(
-            "Unknown project type: `{}`",
-            project_type
-        )),
+        "rust" => init_rust(config, dir),
+        _ => Err(Error::UnknownProjectType(project_type.to_string()).into()),
     }
 }
 
-fn init_rust(dir: &str) -> Result<(), failure::Error> {
-    run_cmd("cargo", &["init", "--bin", dir], Verbosity::Silent)?;
+fn init_rust(config: &Config, dir: &str) -> Result<(), failure::Error> {
+    run_cmd(config, "cargo", &["init", "--bin", dir], Verbosity::Silent)?;
     // TODO: should clone starter repo with `.cargo/config` and whatnot
     // also ensure that compiler plugin is insstalled
     Ok(())
