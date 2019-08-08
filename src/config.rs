39af2a18b90869064caa3cb5496c4a85b23ef941
Nick Hynes <nhynes@oasislabs.com>
2019-08-08 23:49:55 +0000

    Fix default config toml

--- src/config.rs
+++ src/config.rs
@@ -26,7 +26,7 @@ pub static DEFAULT_GATEWAY_URL: &str = default_gateway_url!();
 #[rustfmt::skip]
 static DEFAULT_CONFIG_TOML: &str = concat!(r#"
 [profile.default]
-endpoint = ""#, default_gateway_url!(), r#"
+endpoint = ""#, default_gateway_url!(), r#""
 
 [profile.local]
 mnemonic = "range drive remove bleak mule satisfy mandate east lion minimum unfold ready"
