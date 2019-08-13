bd1343f1e170f4857849dbec32a1c8e16031c61a
sharmaster96 <ennsharma@oasislabs.com>
2019-08-12 21:34:18 -0500

    Change deploy link

--- src/subcommands/deploy.rs
+++ src/subcommands/deploy.rs
@@ -1,5 +1,5 @@
 pub fn deploy() -> Result<(), failure::Error> {
-    match webbrowser::open("https://dashboard.oasiscloud.io/newcontract") {
+    match webbrowser::open("https://dashboard.oasiscloud.io/services?new") {
         Ok(_) => Ok(()),
         Err(err) => Err(failure::format_err!(
             "failed to open browser for service deployment: {}",
